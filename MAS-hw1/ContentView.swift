145be47eeca4ecd7d9a78fd737c4dbc02b1890dc
zirongGatech <zroyu@gatech.edu>
2021-09-10 22:30:13 -0400

    switch to SwiftUI

---
+++ MAS-hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MAS-hw1
+//
+//  Created by Zirong Yu on 9/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MAS-hw1/MAS_hw1App.swift
@@ -0,0 +1,17 @@
+//
+//  MAS_hw1App.swift
+//  MAS-hw1
+//
+//  Created by Zirong Yu on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MAS_hw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
