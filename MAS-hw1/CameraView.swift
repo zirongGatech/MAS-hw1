edbd374bf315117ff94f3aa46893382443f334ad
Ren Liu <rliu384@gatech.edu>
2021-09-11 03:51:08 -0400

    cameraViewUpdate: select photo

--- MAS-hw1/CameraView.swift
+++ MAS-hw1/CameraView.swift
@@ -15,27 +15,36 @@ struct CameraView: View {
     @State private var sourceType: UIImagePickerController.SourceType
         = .camera
     
+    @State var image: UIImage?
+    
     var body: some View {
         
         NavigationView {
             VStack{
                 Text("There is an image")
+                Image(uiImage: image ?? UIImage(named: "placeholder")!)
+                    .resizable().frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 
                 Button("Choose Picture"){
                     self.showSheet = true
                 }.padding()
                 .actionSheet(isPresented: $showSheet, content: {
-                    ActionSheet(title: Text("Select Photo"), message: Text("Choose"), buttons: [.default(Text("Photo Library")){
-                        
+                    ActionSheet(title: Text("Select Photo"), message: Text("Choose"), buttons: [
+                    .default(Text("Photo Library")){
+                        self.showImagePicker = true
+                        self.sourceType = .photoLibrary
                     },
                     .default(Text("Camera")) {
-                        
+                        self.showImagePicker = true
+                        self.sourceType = .camera
                     },
                     .cancel()])
                 })
                 
                 .navigationTitle("Camera View")
             }
+        }.sheet(isPresented: $showImagePicker){
+            ImagePicker(image: $image, isShown: self.$showImagePicker, sourceType: self.sourceType)
         }
     }
 }
